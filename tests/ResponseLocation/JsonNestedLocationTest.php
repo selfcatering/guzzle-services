7b2cb10c724d32c9ec86836f34fbc9b1f84faf23
selfcatering <admin@selfcatering.travel>
2017-02-10 14:36:41 +0100

    [WIP] wrap single element arrays with array to keep models clean

--- tests/ResponseLocation/JsonNestedLocationTest.php
+++ tests/ResponseLocation/JsonNestedLocationTest.php
@@ -15,14 +15,45 @@ use GuzzleHttp\Psr7\Response;
  * @covers \GuzzleHttp\Command\Guzzle\ResponseLocation\JsonLocation
  * @covers \GuzzleHttp\Command\Guzzle\Deserializer
  */
-class JsonLocationTest extends \PHPUnit_Framework_TestCase
+class JsonNestedLocationTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @group ResponseLocation
      */
     public function testVisitsNestedArrayOfObjects()
     {
-        $json = json_decode('{"scalar":"foo","nester":{"nested":[{"foo":111,"bar":123,"baz":false},{"foo":222,"bar":345,"baz":true},{"foo":333,"bar":678,"baz":true}]}}');
+        $json = json_decode('{"Hotels":[{"HotelFees":{"@size":"1","HotelFee":{"@description":"MandatoryTax","@amount":"12.33"}}},{"HotelFees":{"@size":"2","HotelFee":[{"@description":"MandatoryTax","@amount":"13.96"},{"@description":"ResortFee","@amount":"14.09"}]}}]}');
+
+        /*
+        [
+            'Hotels' => [
+                0 => [
+                    'HotelFees' => [
+                        '@size' => '1',
+                        'HotelFee' => [
+                            '@description' => 'MandatoryTax',
+                            '@amount' => '12.33',
+                        ],
+                    ],
+                ],
+                1 => [
+                    'HotelFees' => [
+                        '@size' => '2',
+                        'HotelFee' => [
+                            0 => [
+                                '@description' => 'MandatoryTax',
+                                '@amount' => '13.96',
+                            ],
+                            1 => [
+                                '@description' => 'ResortFee',
+                                '@amount' => '14.09',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ]
+         */
 
         $body = \GuzzleHttp\json_encode($json);
         $response = new Response(200, ['Content-Type' => 'application/json'], $body);
@@ -43,54 +74,75 @@ class JsonLocationTest extends \PHPUnit_Framework_TestCase
                     'type' => 'object',
                     'location' => 'json',
                     'properties' => [
-                        'scalar' => [
-                            'type' => 'string'
-                        ],
-                        'nester' => [
-                            'type' => 'object',
-                            'location' => 'json',
-                            'nested' => [
+                        'Hotels' => [
+                            'type' => 'array',
+                            'items' => [
                                 'type' => 'object',
-                                'location' => 'json',
-                                'items' => [
-                                    'type' => 'object',
-                                    'properties' => [
-                                        'bar' => [
-                                            'type' => 'string',
+                                'properties' => [
+                                    'HotelFees' => [
+                                        'type' => 'object',
+                                        'properties' => [
+                                            '@size' => [
+                                                'type' => 'integer',
+                                            ],
+                                            'HotelFee' => [
+                                                'type' => 'array',
+                                                'items' => [
+                                                    'type' => 'object',
+                                                    '@description' => [
+                                                        'type' => 'string',
+                                                    ],
+                                                    '@amount' => [
+                                                        'type' => 'numeric',
+                                                    ],
+                                                ],
+                                            ],
                                         ],
-                                        'baz' => [
-                                            'type' => 'string',
-                                        ],
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ]
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
         ]);
 
         $guzzle = new GuzzleClient($httpClient, $description);
         /** @var ResultInterface $result */
         $result = $guzzle->foo();
+
+        var_dump($result->toArray());
+        die();
+
         $expected = [
-            'scalar' => 'foo',
-            'nester' => [
-                'nested' => [
-                    [
-                        'bar' => 123,
-                        'baz' => false,
+            'Hotels' => [
+                [
+                    'HotelFees' => [
+                        '@size' => '1',
+                        'HotelFee' => [
+                            [
+                                '@description' => 'MandatoryTax',
+                                '@amount' => '12.33',
+                            ],
+                        ],
                     ],
-                    [
-                        'bar' => 345,
-                        'baz' => true,
+                ],
+                [
+                    'HotelFees' => [
+                        '@size' => '2',
+                        'HotelFee' => [
+                            [
+                                '@description' => 'MandatoryTax',
+                                '@amount' => '13.96',
+                            ],
+                            [
+                                '@description' => 'ResortFee',
+                                '@amount' => '14.09',
+                            ],
+                        ],
                     ],
-                    [
-                        'bar' => 678,
-                        'baz' => true,
-                    ],
-                ]
-            ]
+                ],
+            ],
         ];
         $this->assertEquals($expected, $result->toArray());
     }
